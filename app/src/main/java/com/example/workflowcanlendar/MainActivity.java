697ad223bae45b7f642d657f530a755b56c01300
mikenosuspend <mikemakeit190923@gmail.com>
2024-12-06 23:48:38 +0800

    back-up

--- app/src/main/java/com/example/workflowcanlendar/MainActivity.java
+++ app/src/main/java/com/example/workflowcanlendar/MainActivity.java
@@ -1,25 +1,41 @@
 package com.example.workflowcanlendar;
 
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.workflowcanlendar.Layout.TaskPreviewWidget;
+import com.example.workflowcanlendar.Repository.AppRepository;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
-    private TaskPreviewWidget taskPreview;
-
+//    private TaskPreviewWidget taskPreview_home, taskPreview_work, taskPreview_school, taskPreview_friends;
+//    private BottomNavigationView bottomNavigationView;
+//    private ImageButton addTaskButton = findViewById(R.id.addTaskButton);
+//    private AppRepository dbRepository;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
-        taskPreview = findViewById(R.id.taskPreviewWidget_1);
+//        dbRepository = new AppRepository(this);
+//
+//        taskPreview_home = findViewById(R.id.taskPreviewWidget_1);
+//        taskPreview_work = findViewById(R.id.taskPreviewWidget_2);
+//        taskPreview_school = findViewById(R.id.taskPreviewWidget_3);
+//        taskPreview_friends = findViewById(R.id.taskPreviewWidget_4);
+//
+//        bottomNavigationView = findViewById(R.id.bottom_navigation);
+//        bottomNavigationView.setSelectedItemId(R.id.nav_home);
+//
+//        addTaskButton.findViewById(R.id.addTaskButton);
 
-        taskPreview.setText("50");
-        taskPreview.setProgress(50);
+//        addTaskButton.setOnClickListener(v -> addTask());
+    }
 
+    private void addTask() {
     }
 }
